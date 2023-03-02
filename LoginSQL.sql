62de26d817a138f07d6a6b4a4e1b9da002c0e48d
Ethan Teather <et365@kent.ac.uk>
2023-03-02 13:46:34 +0000

    updated to work on vs (if your server is connected) and mysql

--- LoginSQL.sql
+++ LoginSQL.sql
@@ -1,10 +1,10 @@
-CREATE TABLE `test` (
-  `username` int NOT NULL,
-  `password` varchar(45) NOT NULL,
-  PRIMARY KEY (`username`,`password`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-INSERT INTO `testing`.`test`
-(`username`,
-`password`)
-VALUES
-('1','o1'),('2','df2'),('6','trer12');
+DROP TABLE IF EXISTS test;
+CREATE TABLE test (
+  username int NOT NULL,
+  password varchar(45) NOT NULL,
+  PRIMARY KEY (username,password)
+);
+-- ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+insert into test
+values ('0', 't0'), ('1', 't1'), ('2', 'otest')
