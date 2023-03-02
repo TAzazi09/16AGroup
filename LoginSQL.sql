d504065141e2e7959ba97dfdbd339469d7ec8861
Ethan Teather <et365@kent.ac.uk>
2023-03-02 13:25:00 +0000

    added new sql for testing of logging in

---
+++ LoginSQL.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `test` (
+  `username` int NOT NULL,
+  `password` varchar(45) NOT NULL,
+  PRIMARY KEY (`username`,`password`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+INSERT INTO `testing`.`test`
+(`username`,
+`password`)
+VALUES
+('1','o1'),('2','df2'),('6','trer12');
