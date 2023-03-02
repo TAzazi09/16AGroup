82c495bdd16a334ee1f8ebafeff63cd9875e2dbf
Ethan Teather <et365@kent.ac.uk>
2023-03-02 17:12:39 +0000

    Updated sql for patient table, as well as added test case for first name

--- Registration.java
+++ Registration.java
@@ -1,5 +1,11 @@
--public class Registration {
+import java.lang.RuntimeException;
+
+import javax.management.RuntimeErrorException;
+public class Registration {
     public static void main(String FirstName, String Surname, Character Gender, Integer age, String PhoneNumber, String DoctorChosen, String Details) {
-        if (FirstName = )
+        if (FirstName.length() > 15 )
+        {
+            throw new RuntimeException("First name too long!")
+        }
     }
 }
