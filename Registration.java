bf2ff5eea9700055cdc94350f1e9ee6debc9c680
Ethan Teather <et365@kent.ac.uk>
2023-03-02 17:55:14 +0000

    Added sql for Registration

--- Registration.java
+++ Registration.java
@@ -1,5 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.print.Doc;
+
 public class Registration {
-    public static void main(String FirstName, String Surname, String Gender, Integer Age, String PhoneNumber, String DoctorChosen, String Details) {
+    private static Connection connection;
+    private static Statement statement;
+    private static ResultSet resultSet;
+    public static void main(String[] args) {
+        test("Ethan", "Teather", "Male", 19, "11111 111111", "Me", "Not Known" );
+    }
+    public static void test(String FirstName, String Surname, String Gender, Integer Age, String PhoneNumber, String DoctorChosen, String Details) {
         if (FirstName.length() > 15 )
         {
             throw new RuntimeException("First name too long!");
@@ -30,5 +43,20 @@ public class Registration {
         }
 
         System.out.println("Correct! Sending data!");
+    
+
+    try {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost/testing?user=et365&password=legome12");
+        statement = connection.createStatement();
+        resultSet = statement.executeQuery("insert into patients (FirstName, Surname, Gender, Age, PhoneNumber, DoctorChosen, Values )"
+        + "values (" + FirstName + "," + Surname + "," + Gender  + "," + Age + "," + PhoneNumber + "," + DoctorChosen + "," + Details + ";" + 
+        "select * from patients");
+       // boolean stop = false;
+        while (resultSet.next())
+        System.out.println(resultSet.getString("PatientID") + " - " + resultSet.getString("FirstName") + " - " + resultSet.getString("Surname"));
+    } catch (Exception e) {
+        e.printStackTrace();
     }
+}
 }
--- Registration.sql
+++ Registration.sql
@@ -1,12 +1,12 @@
-DROP TABLE IF EXISTS test;
+DROP TABLE IF EXISTS patients;
 CREATE TABLE patients (
-patientID int not null,
+PatientID int not null,
   FirstName VARCHAR(15) not null,
   Surname varchar(15) NOT NULL,
   PRIMARY KEY (patientID),
   Gender varchar(10),
   Age int (3),
   PhoneNumber varchar(15),
-  DoctorChoson varchar(20),
+  DoctorChosen varchar(20),
   Details varchar(100)
 );
