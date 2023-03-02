35f3f0650e0ffc00c88d2767e3fdb01795af8858
Ethan Teather <et365@kent.ac.uk>
2023-03-02 20:13:22 +0000

    Added autoincrement for patientID

--- Registration.sql
+++ Registration.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS patients;
 CREATE TABLE patients (
-PatientID int not null,
+PatientID int not null auto_increment,
   FirstName VARCHAR(15) not null,
   Surname varchar(15) NOT NULL,
   PRIMARY KEY (patientID),
