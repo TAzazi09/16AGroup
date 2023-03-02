cc0ef77c8e126ad70ef4a2330e3078687f0deacc
Ethan Teather <et365@kent.ac.uk>
2023-03-02 16:56:39 +0000

    New file which checks the data provided from registration interface and sends the data if its validated

---
+++ Registration.java
@@ -0,0 +1,5 @@
+public class Registration {
+    public static void main(String FirstName, String Surname, Character Gender, Integer age, String PhoneNumber, String DoctorChosen, String Details) {
+        if (FirstName = )
+    }
+}
