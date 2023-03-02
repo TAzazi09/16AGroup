8c5d3265a181bd5c0f0adf69edff52bebd09915c
Ethan Teather <et365@kent.ac.uk>
2023-03-02 17:35:59 +0000

    Added more validation in .java

--- Registration.java
+++ Registration.java
@@ -1,11 +1,34 @@
-import java.lang.RuntimeException;
-
-import javax.management.RuntimeErrorException;
 public class Registration {
-    public static void main(String FirstName, String Surname, Character Gender, Integer age, String PhoneNumber, String DoctorChosen, String Details) {
+    public static void main(String FirstName, String Surname, String Gender, Integer Age, String PhoneNumber, String DoctorChosen, String Details) {
         if (FirstName.length() > 15 )
         {
-            throw new RuntimeException("First name too long!")
+            throw new RuntimeException("First name too long!");
         }
+        else if (Surname.length() > 15 )
+        {
+            throw new RuntimeException("Surname too long!");
+        }
+        else if (Age > 120 )
+        {
+            throw new RuntimeException("Invalid age!");
+        }
+        else if (Age < 18 )
+        {
+            throw new RuntimeException("You need to be 18 to register!");
+        }
+        else if (!PhoneNumber.matches("\\d{5} \\d{6}"))
+        {
+            throw new RuntimeException("Invalid phone number");
+        }
+        // else if (DoctorChosen.length() > 15 )
+        // {
+        //     throw new RuntimeException("First name too long!")
+        // }
+        else if (Details.length() > 100 )
+        {
+            throw new RuntimeException("First name too long!");
+        }
+
+        System.out.println("Correct! Sending data!");
     }
 }
