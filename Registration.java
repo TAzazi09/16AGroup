fca8fe1292ae361cc759c6f0758de03bcdca7898
Ethan Teather <et365@kent.ac.uk>
2023-03-02 17:02:11 +0000

    update

--- Registration.java
+++ Registration.java
@@ -1,4 +1,4 @@
-public class Registration {
+-public class Registration {
     public static void main(String FirstName, String Surname, Character Gender, Integer age, String PhoneNumber, String DoctorChosen, String Details) {
         if (FirstName = )
     }
