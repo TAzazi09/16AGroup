2339ddaac1776e36df3a886d8d7a7819716ff4f8
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2023-03-07 19:15:08 +0000

    Integrated with application.java
    1. Converted two methods to static (testFunc and nextStep)
    2. Deleted unnecessary var declaration (u = user && p = password)
    3. Fixed spacing issues

--- LoginCheck.java
+++ LoginCheck.java
@@ -9,16 +9,13 @@ public class LoginCheck {
     private static ResultSet resultSet;
 
     public static void main(String user, String password) {
-        LoginCheck db = new LoginCheck();
-        String u = user;
-        String p = password;
-        db.testFunction(u,p);
+        testFunction(user, password);
+
         System.out.println("username is " + user);
         System.out.println("password is " + password);
     }
 
-    public void testFunction(String username, String password) {
-
+    public static void testFunction(String username, String password) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection("jdbc:mysql://localhost/testing?user=***&password=***");
@@ -26,26 +23,25 @@ public class LoginCheck {
             resultSet = statement.executeQuery("select * from test where username = '" + username + "' and password = '"
                     + password + "'");
             boolean stop = false;
+            
             while ((resultSet.next()) || (stop = false)) {
-
                 if ((username.equals(resultSet.getString("username")))
                         || (password.equals(resultSet.getString("password")))) {
                     stop = true;
                     nextStep();
                     break;
-
                 }
             }
+            
             if (!stop) {
                 System.out.println("Fails");
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 
-    public void nextStep() {
+    public static void nextStep() {
         System.out.println("this would be the next page");
     }
 }
