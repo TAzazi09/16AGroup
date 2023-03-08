7c42f681cfb9855cf0bf822b90208135e8112d3d
Ethan Teather <et365@kent.ac.uk>
2023-03-08 23:43:56 +0000

    Removed table create since thats opperated through GeneralPage and added new popus

--- Registration.java
+++ Registration.java
@@ -2,8 +2,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.*;
-
+// import GeneralPage.java;
 import javax.print.Doc;
+import javax.swing.JOptionPane;
 import javax.xml.stream.events.StartElement;
 
 public class Registration {
@@ -12,46 +13,45 @@ public class Registration {
     private static ResultSet resultSet;
 
     public static void main(String[] args) {
-        Registration n = new Registration();
-        n.test("John", "ER", "Male", 19, "11111 111111", "Me", "Not Known");
-        // test("girs","er", "male", 12, "00000 000000", "me", "then")
-        //    test(String FirstName, String Surname, String Gender, Integer Age, String PhoneNumber, String DoctorChosen, String Details) {
 
     }
 
-    public void test(String FirstName, String Surname, String Gender, Integer Age, String PhoneNumber,
+    public static boolean test(String FirstName, String Surname, String Gender, Integer Age, String PhoneNumber,
             String DoctorChosen, String Details) {
+        System.out.println(FirstName);
         if (FirstName.length() > 15) {
-            throw new RuntimeException("First name too long!");
+            JOptionPane.showMessageDialog(null, "First name too long!");
+            return false;
         } else if (Surname.length() > 15) {
-            throw new RuntimeException("Surname too long!");
+            JOptionPane.showMessageDialog(null, "Surname too long!");
+            return false;
         } else if (Age > 120) {
-            throw new RuntimeException("Invalid age!");
+            JOptionPane.showMessageDialog(null, "Invalid age!");
+            return false;
         } else if (Age < 18) {
-            throw new RuntimeException("You need to be 18 to register!");
+            JOptionPane.showMessageDialog(null, "You need to be 18 to register!");
+            return false;
         } else if (!PhoneNumber.matches("\\d{5} \\d{6}")) {
-            throw new RuntimeException("Invalid phone number");
+            JOptionPane.showMessageDialog(null, "Invalid phone number");
+            return false;
+        } else if (Details.length() > 100) {
+            JOptionPane.showMessageDialog(null, "First name too long!");
+            return false;
         }
-        // else if (DoctorChosen.length() > 15 )
-        // {
-        // //     throw new RuntimeException("First name too long!")
-        // // }
-        else if (Details.length() > 100) {
-            throw new RuntimeException("First name too long!");
-        }
+        return true;
+    }
 
-        System.out.println("Correct! Sending data!");
-
+    public static void sendData(String FirstName, String Surname, String Gender, Integer Age, String PhoneNumber,
+            String DoctorChosen, String Details) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection("jdbc:mysql://localhost?user=***&password=***");
-            statement.execute("create database NHS");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/nhs?user=root&password=legome12");
             statement = connection.createStatement();
             int n = statement.executeUpdate(
                     "insert into patients (PatientID,FirstName, Surname, Gender, Age, PhoneNumber, DoctorChosen, Details )"
                             + "values (DEFAULT,'" + FirstName + "','" + Surname + "','" + Gender + "','" + Age + "','"
                             + PhoneNumber + "','" + DoctorChosen + "','" + Details + "')");
-            //    boolean stop = false;
+            // boolean stop = false;
             resultSet = statement.executeQuery("select * from patients");
             int l = 0;
             while (resultSet.next()) {
@@ -59,20 +59,12 @@ public class Registration {
                 System.out.println(resultSet.getString("PatientID") + " - " + resultSet.getString("FirstName") + " - "
                         + resultSet.getString("Surname"));
             }
-            if (l > 3) {
-                statement.executeUpdate("drop table patients; ");
-                statement.executeUpdate("CREATE TABLE patients (PatientID int not null auto_increment,"
-                        + "FirstName VARCHAR(15) not null,"
-                        + "Surname varchar(15) NOT NULL,"
-                        + "PRIMARY KEY (patientID),"
-                        + "Gender varchar(10),"
-                        + "Age int (3),"
-                        + "PhoneNumber varchar(15),"
-                        + "DoctorChosen varchar(20),"
-                        + "Details varchar(100));");
-            }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
+        JOptionPane.showMessageDialog(null,
+                "Congratulations " + FirstName + " " + Surname + ", and welcome to the NHS!");
+        new GeneralPage().setVisible(true);
     }
 }
