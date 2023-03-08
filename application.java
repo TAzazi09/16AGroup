129e391615312260c9ead8e51d9c877c596ca80a
Ethan Teather <et365@kent.ac.uk>
2023-03-08 23:55:57 +0000

    Updated

--- application.java
+++ application.java
@@ -188,3 +188,11 @@ public class application extends javax.swing.JFrame {
         });
     }
 }
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new application().setVisible(true);
+            }
+        });
+    }
+}
