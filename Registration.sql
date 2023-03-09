39cf78f9f91d2a8718d10e4e04eaccb266bb2487
Ethan Teather <et365@kent.ac.uk>
2023-03-09 10:03:44 +0000

    Updated sql with null

--- Registration.sql
+++ Registration.sql
@@ -4,10 +4,10 @@ PatientID int not null auto_increment,
   FirstName VARCHAR(15) not null,
   Surname varchar(15) NOT NULL,
   PRIMARY KEY (patientID),
-  Gender varchar(10),
-  Age int (3),
+  Gender varchar(10) not null,
+  Age int (3) not null,
   PhoneNumber varchar(15),
-  DoctorChosen varchar(20),
+  DoctorChosen varchar(20) not null,
   Details varchar(100)
 );
 SELECT * from patients
