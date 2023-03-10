ff4313c3ea48b115a2ba90e67d218ec2b1e6826b
Callum Lee <cl659@kent.ac.uk>
2023-03-10 16:14:49 +0000

    Added test cases for registration

---
+++ Functionality/Test/Registration/src/Functionality/RegTest.java
@@ -0,0 +1,85 @@
+package Functionality;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.Assert.assertFalse;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RegTest {
+	
+
+	
+	
+	@Test
+	public void testRegDetailsCorrect() {
+		//Test to check if correct details are accepted
+		assertTrue(Registration.test("james", "lee", "male", 25, "11111 111111", "Dr chan", "deaf & blind"));
+		
+		
+	}
+	@Test
+	public void firstNameTest() {
+		//test to see if it will reject empty string for firstName
+		assertFalse(Registration.test("", "lee", "male", 25, "11111 111111", "Dr chan", "deaf & blind"));
+		//Test over character limit for firstName
+		assertFalse(Registration.test("anhfdsfhdsfhsdfhdsfhfdsh", "lee", "male", 25, "11111 111111", "Dr chan", "deaf & blind"));
+		//Reject numbers in firstName
+		assertFalse(Registration.test("john1245", "lee", "male", 25, "11111 111111", "Dr chan", "deaf & blind"));
+		
+		//Checking for null value
+		assertThrows(NullPointerException.class, () -> 
+		Registration.test(null, "lee", "male", 10, "11111 111111", "Dr chan", "deaf & blind"), "firstName shouldn't be null");
+}
+	
+	
+	@Test
+	public void surnameTest() {
+		
+		//Test surname character limit
+		assertFalse(Registration.test("john", "leeerttysdffdfsdfsdffsds", "male", 25, "11111 111111", "Dr chan", "deaf & blind"));
+		//Checking under limit
+		assertFalse(Registration.test("john", "l", "male", 25, "11111 111111", "Dr chan", "deaf & blind"));
+		//Test for numbers included in surname
+		assertFalse(Registration.test("john", "lee123", "male", 25, "11111 111111", "Dr chan", "deaf & blind"));
+		//test for null value for surname 
+		assertThrows(NullPointerException.class, () -> 
+		Registration.test("john", null, "male", 10, "11111 111111", "Dr chan", "deaf & blind"), "age shoudn't be null");
+}
+	
+	
+	
+	
+	@Test
+	public void ageTest() {
+		//Test under age limit
+		assertFalse(Registration.test("john", "lee", "male", 17, "11111 111111", "Dr chan", "deaf & blind"));
+		//Test over limit
+		assertFalse(Registration.test("john", "lee", "male", 131, "11111 111111", "Dr chan", "deaf & blind"));
+		//Test for null values for age
+		assertThrows(NullPointerException.class, () -> 
+		Registration.test("john", "lee", "male", null, "11111 111111", "Dr chan", "deaf & blind"), "age shoudn't be null");
+	}
+	@Test
+	public void phoneNumberTest() {
+	    //Testing for only correct Phone Number is accepted
+		assertFalse(Registration.test("john", "lee", "male", 25, "111", "Dr chan", "deaf & blind"));
+		//Testing for null values in phone number
+		assertThrows(NullPointerException.class, () -> 
+		Registration.test("john", "lee", "male", 18, null, "Dr chan", "deaf & blind"), "Phone Number shoudn't be null");
+		;
+	}
+
+	
+	
+	@Test
+	public void detailsTest() {
+		//Test to check details only accept under character limit 100
+		assertFalse(Registration.test("james", "lee", "male", 25, "11111 111111", "Dr chan", "deaf & blindsadasdasdasdasdasdggfsafdsdaadssadasdasdsadasdasdsadsadsadsadsadasdasdasddsaasdsadadsasdadsdsaadsasdadsdasasddsasdasdasadsdadsadas"));
+		//Test for null values in detail
+		assertThrows(NullPointerException.class, () -> 
+		Registration.test("john", "lee", "male", 18, "11111 111111", "Dr chan", null), "Detail shoudn't be null");
+		
+	}
+}
---
+++ Functionality/Test/Registration/src/module-info.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+/**
+ * @author Callum Lee
+ *
+ */
+module Registration {
+	requires org.junit.jupiter.api;
+	requires java.desktop;
+	requires junit;
+	requires java.sql;
+}
