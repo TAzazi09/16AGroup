22be092ba723dbaff564682ab61892912b82183c
Ethan Teather <et365@kent.ac.uk>
2023-03-14 17:28:47 +0000

    Finished file to connection to databases

--- Functionality/DatabaseConnection.java
+++ Functionality/DatabaseConnection.java
@@ -1,14 +1,44 @@
 package Functionality;
 
+/**
+ * @author ethan
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Scanner;
 
-public class DatabaseConnection
-{
-public static void main(String[] args) {
-    System.out.println("Please enter your password: ");
-    Scanner test = new Scanner(System.in);
-    String next = test.nextLine();
-    System.out.println(next);
+import javax.swing.JOptionPane;
 
-}
+public class DatabaseConnection {
+    public static Connection connection;
+    private static Statement statement;
+
+    public static void main(String[] args) {
+        running();
+    }
+
+    // connection =
+    // DriverManager.getConnection("jdbc:mysql://localhost/nhs?user=root&password=***");
+    public static void running() {
+        System.out.println("Please enter your password: ");
+        Scanner test = new Scanner(System.in);
+        String next = test.nextLine();
+        test.close(); 
+        String root = "root";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager
+                    .getConnection("jdbc:mysql://localhost/", root, next);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        getConnection();
+    }
+
+    public static Connection getConnection() {
+        return connection;
+    }
 }
