97c99db76880d1706fe58a8a82a0628149c64c8f
Ethan Teather <et365@kent.ac.uk>
2023-03-14 17:30:16 +0000

    Removed unused imports and declerations

--- Functionality/DatabaseConnection.java
+++ Functionality/DatabaseConnection.java
@@ -6,15 +6,11 @@ package Functionality;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.Scanner;
 
-import javax.swing.JOptionPane;
 
 public class DatabaseConnection {
     public static Connection connection;
-    private static Statement statement;
 
     public static void main(String[] args) {
         running();
