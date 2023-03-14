34f5278bf5896ba3e374c9a263182841ae2e48d1
Ethan TEather <et365@raptor.kent.ac.uk>
2023-03-14 13:38:08 +0000

    Added connection used by database

---
+++ Functionality/DatabaseConnection.java
@@ -0,0 +1,14 @@
+package Functionality;
+
+import java.util.Scanner;
+
+public class DatabaseConnection
+{
+public static void main(String[] args) {
+    System.out.println("Please enter your password: ");
+    Scanner test = new Scanner(System.in);
+    String next = test.nextLine();
+    System.out.println(next);
+
+}
+}
