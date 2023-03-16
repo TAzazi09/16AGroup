9a57136f470d29c5acf336c4cfd145fbaa8a3bec
Ethan Teather <et365@kent.ac.uk>
2023-03-16 18:18:09 +0000

    Added functions to get firstname and surname

--- Functionality/LoginCheck.java
+++ Functionality/LoginCheck.java
@@ -13,6 +13,8 @@ import javax.swing.JOptionPane;
 
 public class LoginCheck {
     public static String backgroundID;
+    public static String fname;
+    public static String sname;
     public static void main(String user, String password) {
         testFunction(user, password);
     }
@@ -38,6 +40,8 @@ public class LoginCheck {
                 if ((username.equals(resultSet.getString("FirstName")))
                         && (password.equals((resultSet.getString("Surname")) + (resultSet.getString("PatientID"))))) {
                     backgroundID = resultSet.getString("patientID");
+                    fname = resultSet.getString("FirstName");
+                    sname = resultSet.getString("Surname");
                     JOptionPane.showMessageDialog(null, "Welcome to the NHS!");
                     Window[] windows = Window.getWindows();
 
@@ -62,5 +66,13 @@ public class LoginCheck {
 
     public static String getID() {
         return backgroundID;
+    }
+
+    public static String getFirstName() {
+        return fname;        
+    }
+
+    public static String getSurname() {
+        return sname;
     }
 }
