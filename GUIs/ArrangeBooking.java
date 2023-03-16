ff49c3dcc18fd2605af8aa08f72265e521773bd2
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2023-03-16 18:47:28 +0000

    Java-style instead of C-style main method

--- GUIs/ArrangeBooking.java
+++ GUIs/ArrangeBooking.java
@@ -11,7 +11,7 @@ public class ArrangeBooking extends javax.swing.JFrame {
         initComponents();
     }
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         /* Set the Nimbus look and feel */
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
