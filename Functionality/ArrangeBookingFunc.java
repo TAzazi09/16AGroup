c2c6c3c60e2b9fa3ec496c25113268cc8e455f58
Ethan TEather <et365@raptor.kent.ac.uk>
2023-03-19 20:35:54 +0000

    Updated function to work

--- Functionality/ArrangeBookingFunc.java
+++ Functionality/ArrangeBookingFunc.java
@@ -15,21 +15,35 @@ public class ArrangeBookingFunc {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
 
-            //Connects to the database
+            // Connects to the database
             Connection connection = DatabaseConnectionFunc.getConnection();
             Statement statement = connection.createStatement();
 
-            //Test input
-            ResultSet results = statement
-                    .executeQuery("SELECT * FROM bookings WHERE Time = '" + time + "' AND Date = '" + date + "'");
+            // Test input
+            ResultSet currentDoctor = statement
+                    .executeQuery("SELECT DoctorChosen FROM patients WHERE patientID = '" + LoginCheck.getID() + "';");
+            // if ((currentDoctor.getString("DoctorChosen")) == null) {
+            //     // JOptionPane.showMessageDialog("Booking completed "");
+            // } else { 
+                currentDoctor.next();
+                String doctor = currentDoctor.getString("DoctorChosen");
+                ResultSet results = statement
+                        .executeQuery("SELECT * FROM bookings WHERE Time = '" + time + "' AND DoctorChosen = '"
+                                + doctor + "' AND Date = '" + date + "';");
+            // }
             if (results.next()) {
-                JOptionPane.showMessageDialog(null, "INSERT DOCTOR NAME is unavailable at that time.");
+                JOptionPane.showMessageDialog(null, doctor + " is unavailable at that time.");
             } else {
+                statement.execute(
+                    "INSERT INTO Bookings (PatientID, DoctorChosen, Time, Date) VALUES ('" + LoginCheck.getID() +  "' , '" + doctor + "', '" + time + "' , '" + date + "');"
+                );
+                
                 JOptionPane.showMessageDialog(null,
                         "Your booking has successfully been arranged at " + time + " on the " + date + ".");
                 statement.execute("UPDATE patients SET messages = CONCAT(messages,'\n + " + LoginCheck.getFirstName()
                         + " " + LoginCheck.getSurname() + " has arranged a booking at "
-                        + time + " on " + date + ".') WHERE patientID = '" + LoginCheck.getID() + "';");
+                        + time + " on " + date + " with " + doctor + ".') WHERE patientID = '"
+                        + LoginCheck.getID() + "';");
                 Window[] windows = Window.getWindows();
 
                 // Close all windows in the array
