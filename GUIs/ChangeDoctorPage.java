30e9add7584aecdd6dca5d27c8db62ddfefca7b9
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2023-03-21 17:56:50 +0000

    Major ChangeDoctorPage reformatting
    Big changes:
    Converted all fields to local vars
    converted a new java.awt.event.ActionListener() call to a method reference
    converted a new Runnable() call to a lambda method
    
    Minor changes:
    removed curDocID initializer
    changed a C-style to a Java-style array declaration (in main)

--- GUIs/ChangeDoctorPage.java
+++ GUIs/ChangeDoctorPage.java
@@ -13,15 +13,6 @@ import Databases.DoctorsDB;
  * @author Nikola
  */
 public class ChangeDoctorPage extends javax.swing.JFrame {
-    private javax.swing.JButton backButton;
-    private javax.swing.JLabel changeDocLabel;
-    private javax.swing.JLabel curDocLabel;
-    private javax.swing.JLabel curDocName;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JComboBox<String> newDocBox;
-    private javax.swing.JLabel newDocLabel;
-    private javax.swing.JButton submitButton;
-
     public ChangeDoctorPage() {
         initComponents();
     }
@@ -29,14 +20,14 @@ public class ChangeDoctorPage extends javax.swing.JFrame {
     private void initComponents() {
         getContentPane().setBackground(new java.awt.Color(255, 255, 255));
 
-        jPanel1 = new javax.swing.JPanel();
-        changeDocLabel = new javax.swing.JLabel("Change your doctor");
-        newDocBox = new javax.swing.JComboBox<>();
-        curDocLabel = new javax.swing.JLabel("Current doctor: ");
-        newDocLabel = new javax.swing.JLabel("New doctor: ");
-        curDocName = new javax.swing.JLabel("doc name");
-        backButton = new javax.swing.JButton("Back");
-        submitButton = new javax.swing.JButton("Submit");
+        javax.swing.JPanel jPanel1 = new javax.swing.JPanel();
+        javax.swing.JLabel changeDocLabel = new javax.swing.JLabel("Change your doctor");
+        javax.swing.JComboBox<String> newDocBox = new javax.swing.JComboBox<>();
+        javax.swing.JLabel curDocLabel = new javax.swing.JLabel("Current doctor: ");
+        javax.swing.JLabel newDocLabel = new javax.swing.JLabel("New doctor: ");
+        javax.swing.JLabel curDocName = new javax.swing.JLabel("doc name");
+        javax.swing.JButton backButton = new javax.swing.JButton("Back");
+        javax.swing.JButton submitButton = new javax.swing.JButton("Submit");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -54,7 +45,7 @@ public class ChangeDoctorPage extends javax.swing.JFrame {
 
         curDocName.setFont(new java.awt.Font("Monospaced", Font.PLAIN, 18));
 
-        int curDocID = 0;
+        int curDocID;
         String curDoc = "";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -70,7 +61,7 @@ public class ChangeDoctorPage extends javax.swing.JFrame {
             curDocID = Integer.parseInt(currentDoctor.getString("DoctorID"));
             curDoc = DoctorsDB.getDoctorName(curDocID);
 
-            // get the list of doctors without the current patient's doctor (for the drop down menu)
+            // get the list of doctors without the current patient's doctor (for the drop-down menu)
             ResultSet doctors = statement.executeQuery("SELECT * FROM doctors WHERE DoctorID != '" + curDocID + "';");
             while (doctors.next()) {
                 newDocBox.addItem(doctors.getString("Name"));
@@ -88,11 +79,7 @@ public class ChangeDoctorPage extends javax.swing.JFrame {
 
         submitButton.setFont(new java.awt.Font("Monospaced", Font.PLAIN, 18));
         submitButton.setBackground(buttonBlue);
-        submitButton.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                submitButtonActionPerformed(evt);
-            }
-        });
+        submitButton.addActionListener(this::submitButtonActionPerformed);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -170,7 +157,7 @@ public class ChangeDoctorPage extends javax.swing.JFrame {
         dispose();
     }
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         /* Set the Nimbus look and feel */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
@@ -194,10 +181,6 @@ public class ChangeDoctorPage extends javax.swing.JFrame {
         }
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new ChangeDoctorPage().setVisible(true);
-            }
-        });
+        java.awt.EventQueue.invokeLater(() -> new ChangeDoctorPage().setVisible(true));
     }
 }
