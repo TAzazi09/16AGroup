72f1c4bfec0d490a13206135c472ab84f5d4bfb0
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2023-03-22 18:58:49 +0000

    Added connection and statement to Session.Info

--- Session/Info.java
+++ Session/Info.java
@@ -1,8 +1,33 @@
 package Session;
 
+import java.sql.Connection;
+import java.sql.Statement;
+import Functionality.DatabaseConnectionFunc;
+
 /**
  * @author Nikola
  */
 public class Info {
+    public static boolean connected;
+    public static boolean loggedIn;
+
     public static String backgroundID;
+
+    public static Connection connection;
+    public static Statement statement;
+
+    public static void connect() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Connects to the database
+            connection = DatabaseConnectionFunc.getConnection();
+            statement = connection.createStatement();
+
+            // Sets the connected variable to true
+            connected = true;
+        } catch (Exception e) {
+            connected = false;
+        }
+    }
 }
