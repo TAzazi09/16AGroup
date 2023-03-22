42394cf6c9d5c109c023d829e99d4aeb2e89195d
Ethan Teather <et365@kent.ac.uk>
2023-03-22 15:56:01 +0000

    Completed reschedule Functionality.

--- Functionality/ReschedulingFunc.java
+++ Functionality/ReschedulingFunc.java
@@ -12,36 +12,38 @@ import Databases.DoctorsDB;
  * @author Ethan
  */
 public class ReschedulingFunc {
-    public static void resechduleBooking(String oldTime, String oldDate, String newTime, String newDate) {
-        // System.out.println(time);
-        // System.out.println(date);
-
+    public static void resechduleBooking(String oldDate, String oldTime, String newDate, String newTime) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
 
-            //Connects to the database
+            // Connects to the database
             Connection connection = DatabaseConnectionFunc.getConnection();
             Statement statement = connection.createStatement();
-            //Test input
-            System.out.println(LoginCheck.getID());
-            ResultSet D = statement.executeQuery("SELECT DoctorID FROM patients WHERE PatientID = '1'");
+            ResultSet D = statement
+                    .executeQuery("SELECT DoctorID FROM patients WHERE PatientID = '" + LoginCheck.getID() + "'");
+            if (D.next()) {
+                System.out.println("id is " + D.getString("DoctorID"));
+            }
             String currentDoctor = DoctorsDB.getDoctorName(Integer.parseInt(D.getString("DoctorID")));
             int currentDoctorID = Integer.parseInt(D.getString("DoctorID"));
-            // statement.execute(
-            //         "INSERT INTO Bookings (PatientID, BookingID, DoctorID, Time, Date, Detail, Prescription) VALUES ('1', '1', '"
-            //                 + D + "' '12:12', '1212/12/12', 'test', 'other test');");
+            //Checks if there are any bookings that could clash
             ResultSet results = statement
-                    .executeQuery("SELECT * FROM bookings WHERE Time = '" + newTime + "' AND DoctorChosen = '" + currentDoctorID + "' AND Date = '" + newDate + "'");
+                    .executeQuery("SELECT * FROM bookings WHERE Time = '" + newTime + "' AND DoctorID = '"
+                            + currentDoctorID + "' AND Date = '" + newDate + "'");
             if (results.next()) {
+                //Informs the user of the clashes
                 JOptionPane.showMessageDialog(null, currentDoctor + " is unavailable at that time.");
             } else {
                 JOptionPane.showMessageDialog(null,
-                        "Your booking has successfully been changed to " + newTime + " on the " + newTime + ".");
+                        "Your booking has successfully been changed to " + newTime + " on the " + newDate + ".");
                 statement.execute("UPDATE patients SET messages = CONCAT(messages,'\n + " + LoginCheck.getFirstName()
-                        + " " + LoginCheck.getSurname() + " has changed their booking from " + oldTime + " " + oldDate + " to "
-                        + newTime + " " + newTime + ".') WHERE patientID = '" + LoginCheck.getID() + "';");
-
-                // statement.executeUpdate("UPDATE bookings ")        
+                        + " " + LoginCheck.getSurname() + " has changed their booking from " + oldTime + " " + oldDate
+                        + " to "
+                        + newTime + " " + newDate + ".') WHERE patientID = '" + LoginCheck.getID() + "';");
+                //Updates the booking
+                statement.executeUpdate(
+                        "UPDATE bookings SET Date = '" + newDate + "', Time = '" + newTime + "' WHERE Time = '"
+                                + oldTime + "' AND DoctorID = '" + currentDoctorID + "' AND Date = '" + oldDate + "'");
                 Window[] windows = Window.getWindows();
 
                 // Close all windows in the array
