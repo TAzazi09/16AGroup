f03f109f2be624509aaceb13bcbafeb34ca5ed3a
Ethan Teather <et365@kent.ac.uk>
2023-03-22 16:33:12 +0000

    Added database connections

--- Functionality/ChangeDoctorFunc.java
+++ Functionality/ChangeDoctorFunc.java
@@ -1,5 +1,22 @@
 package Functionality;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class ChangeDoctorFunc {
+    public static void main(String[] args) {
+        
     
+    try {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        // Connects to the database
+        Connection connection = DatabaseConnectionFunc.getConnection();
+        Statement statement = connection.createStatement();
+        ResultSet D = statement
+                .executeQuery("SELECT DoctorID FROM patients WHERE PatientID = '" + LoginCheck.getID() + "'");
+       D.next();
+}
+    }
 }
