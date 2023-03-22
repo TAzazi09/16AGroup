3b88e6efebbb036cb4c0ad043315cdc779a1827d
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2023-03-22 18:59:18 +0000

    Connected to Session.Info for connection and statement

--- Databases/DatabaseDB.java
+++ Databases/DatabaseDB.java
@@ -3,6 +3,7 @@ package Databases;
 import java.sql.Connection;
 import java.sql.Statement;
 import Functionality.DatabaseConnectionFunc;
+import Session.Info;
 
 /**
  * @written by Ethan
@@ -12,13 +13,16 @@ public class DatabaseDB {
     // Creates the database
     public static void main(String[] args) {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            if (!Info.connected) {
+                Info.connect();
+            }
+            // Class.forName("com.mysql.cj.jdbc.Driver");
 
-            Connection connection = DatabaseConnectionFunc.getConnection();
-            Statement statement = connection.createStatement();
+            // Connection connection = DatabaseConnectionFunc.getConnection();
+            // Statement statement = connection.createStatement();
 
-            statement.executeUpdate("DROP DATABASE IF EXISTS NHS;");
-            statement.executeUpdate("CREATE DATABASE NHS");
+            Info.statement.executeUpdate("DROP DATABASE IF EXISTS NHS;");
+            Info.statement.executeUpdate("CREATE DATABASE NHS");
 
             // Creates the Doctors, Patients, and Bookings tables
             DoctorsDB.main(null);
