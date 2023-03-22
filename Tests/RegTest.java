3e9ab23ec4c2259b3a1d1976ce6f08fdf9a3c581
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2023-03-22 15:25:47 +0000

    Update RegTest.java
    Added gaps between comments
    
    This is also a test commit because I want to make sure that everything will work after restarting VS code

--- Tests/RegTest.java
+++ Tests/RegTest.java
@@ -10,28 +10,29 @@ import Functionality.RegistrationCheck;
  */
 class RegTest {
     @Test
-    public void testRegDetailsCorrect() {
+    public void correctEverything() {
         //Test to check if correct details are accepted
         assertTrue(RegistrationCheck.test("James", "Lee", "male", 25, "11111 111111", "Dr Andrew", "deaf & blind"));
     }
 
     @Test
-    public void firstNameTest() {//test to see if it will reject empty string for firstName
+    // Test to see if it will reject empty string for firstName
+    public void firstNameTest() {
         assertFalse(RegistrationCheck.test("", "Lee", "male", 25, "11111 111111", "Dr Jason", "deaf & blind"));
         assertFalse(RegistrationCheck.test("", "Brown", "Female", 20, "12345 123456", "Dr Jason", null));
         assertFalse(RegistrationCheck.test("", "Davis", "Female", 83, "07836 142516", "Dr Andrew", ""));
 
-        //Test to see if it will reject null value for firstName
+        // Test to see if it will reject null value for firstName
         assertFalse(RegistrationCheck.test(null, "Kolev", "Male", 18, "98765 098765", "Dr Jason", "foo"));
         assertFalse(RegistrationCheck.test(null, "Smith", "Male", 45, "12345 123456", "Dr Andrew", "bar"));
 
-        //Test to see if it will accept correct firstName
+        // Test to see if it will accept correct firstName
         assertTrue(RegistrationCheck.test("John", "Green", "male", 27, "13579 246800", "Dr Jason", ""));
         assertTrue(RegistrationCheck.test("Jane", "Brown", "Female", 20, "12345 123456", "Dr Jason", null));
         assertTrue(RegistrationCheck.test("Mary", "Davis", "Female", 83, "07836 142516", "Dr Andrew", ""));
         assertTrue(RegistrationCheck.test("John", "Kolev", "Male", 18, "98765 098765", "Dr Jason", "foo"));
 
-        //Test over character limit for firstName
+        // Test over character limit for firstName
         assertFalse(RegistrationCheck.test("Anhfdsfhdsfhsdfhdsfhfdsh", "Lee", "male", 25, "11111 111111", "Dr Jason",
                 "deaf & blind"));
         assertFalse(RegistrationCheck.test("Qwertyuiopasdfghjklzxcvb", "Smith", "Male", 45, "12345 123456", "Dr Andrew",
@@ -39,63 +40,63 @@ class RegTest {
         assertFalse(RegistrationCheck.test("Johnisaveryveryverylongname", "Smith", "Male", 45, "12345 123456",
                 "Dr Andrew", "hello"));
 
-        //Reject numbers in firstName
+        // Reject numbers in firstName
         assertFalse(RegistrationCheck.test("John1245", "Lee", "male", 25, "11111 111111", "Dr Jason", "deaf & blind"));
         assertFalse(RegistrationCheck.test("12345678", "Smith", "Male", 45, "12345 123456", "Dr Andrew", "something"));
         assertFalse(RegistrationCheck.test("123", "Smith", "Male", 45, "12345 123456", "Dr Andrew", "hello"));
 
-        //Reject firstName under character limit
+        // Reject firstName under character limit
         assertFalse(RegistrationCheck.test("J", "Lee", "Male", 25, "11111 111111", "Dr Jason", "deaf & blind"));
         assertFalse(RegistrationCheck.test("A", "Smith", "Male", 45, "12345 123456", "Dr Andrew", "something"));
 
-        //Checking for null value
+        // Checking for null value
         assertFalse(RegistrationCheck.test(null, "Lee", "female", 100, "15073 84920", "Dr Jason", "deaf & blind"));
     }
 
     @Test
     public void surnameTest() {
-        //Test to see if it will reject empty string for surname
+        // Test to see if it will reject empty string for surname
         assertFalse(RegistrationCheck.test("John", "", "male", 25, "11111 111111", "Dr Jason", ""));
 
-        //Test surname character limit
+        // Test surname character limit
         assertFalse(RegistrationCheck.test("Sam", "leeerttysdffdfsdfsdffsds", "male", 25, "11111 111111", "Dr Jason",
                 "deaf & blind"));
 
-        //Checking under limit
+        // Checking under limit
         assertFalse(RegistrationCheck.test("Bob", "l", "male", 25, "11111 111111", "Dr Jason", ""));
 
-        //Test for numbers included in surname
+        // Test for numbers included in surname
         assertFalse(RegistrationCheck.test("Frank", "lee123", "male", 25, "11111 111111", "Dr Smith", "foo"));
 
-        //test for null value for surname 
+        // Test for null value for surname 
         assertFalse(RegistrationCheck.test("Smith", null, "male", 10, "11111 111111", "Dr Smith", "foo"));
     }
 
     @Test
     public void ageTest() {
-        //Test under-age limit
+        // Test under-age limit
         assertFalse(RegistrationCheck.test("Rachel", "Long", "male", 17, "11111 111111", "Dr Smith", "something"));
 
-        //Test over limit
+        // Test over limit
         assertFalse(RegistrationCheck.test("Daniel", "Lee", "male", 131, "11111 111111", "Dr Smith", null));
 
-        //Test for null values for age
+        // Test for null values for age
         assertFalse(RegistrationCheck.test("John", "Steward", "male", null, "11111 111111", "Dr Smith",
                 "Hello world"));
     }
 
     @Test
     public void phoneNumberTest() {
-        //Testing for only correct Phone Number is accepted
+        // Testing for only correct Phone Number is accepted
         assertFalse(RegistrationCheck.test("Jack", "Lampard", "male", 25, "111", "Dr Smith", ""));
 
-        //Testing for null values in phone number
+        // Testing for null values in phone number
         assertFalse(RegistrationCheck.test("Elon", "Musk", "male", 18, null, "Dr Andrew", null));
     }
 
     @Test
     public void detailsTest() {
-        //Test to check details only accept under character limit 100
+        // Test to check details only accept under character limit 100
         assertFalse(RegistrationCheck.test("James", "Stephan", "male", 25, "11111 111111", "Dr Andrew",
                 "deaf & blindsadasdasdasdasdasdggfsafdsdaadssadasdasdsadasdasdsadsadsadsadsadasdasdasddsaasdsadadsasdadsdsaadsasdadsdasasddsasdasdasadsdadsadas"));
     }
