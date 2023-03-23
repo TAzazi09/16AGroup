21a17e8c6bbabdf738f9ba55b0c135a6cf69ff72
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2023-03-23 09:11:51 +0000

    Create Session.General to store basic info for the app
    - like font and accent color

---
+++ Session/General.java
@@ -0,0 +1,12 @@
+package Session;
+
+import java.awt.Font;
+
+/**
+ * @author Nikola
+ */
+public class General {
+    public static Font font(int size) {
+        return new Font("Monospaced", Font.PLAIN, size);
+    }
+}
