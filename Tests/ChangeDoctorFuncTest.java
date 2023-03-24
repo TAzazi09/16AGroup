99f6e087d330c766588fde1da62e0e2ff3a2c623
Callum Lee <cl659@kent.ac.uk>
2023-03-24 21:02:04 +0000

    Tests for sprint 2 & test specification

---
+++ Tests/ChangeDoctorFuncTest.java
@@ -0,0 +1,109 @@
+package kent.comp5590;
+
+import static org.junit.Assert.*;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import kent.comp5590.*;
+
+/**
+ * @author Callum
+ */
+public class ChangeDoctorFuncTest {
+	
+	 public static Connection connection;
+	 public static Statement statement;
+
+	@Before
+	 public void setUp() throws Exception {
+		
+		try {
+			 Class.forName("com.mysql.cj.jdbc.Driver");
+
+	         DatabaseConnectionFunc.main(null);
+
+	         // Initialize the database
+	         if (DatabaseConnectionFunc.connected) {
+	             DatabaseDB.main(null);
+	         }
+			  } catch (Exception e) {
+		            e.printStackTrace();
+		        }
+	}
+
+	
+	 @Test
+	 public void testChangeDoctor() throws Exception {
+	        // Set up initial state
+	        String oldDoctorName = "Dr. Smith";
+	        String newDoctorName = "Dr. Johnson";
+	        int oldDoctorID = 123;
+	        int newDoctorID = 456;
+	        String backgroundID = "789";
+	        String firstName = "John";
+	        String surname = "Doe";
+	        String expectedMessage = "Your doctor has successfully been changed to " + newDoctorName + ".";
+
+	        
+
+	        // Call the method
+	        Info.backgroundID = backgroundID;
+	        try {
+	        	
+	        	//statement = connection.createStatement();
+	        //Checking if it can change doctor if all requirements are hit	
+	        Info.statement
+            .execute("UPDATE patients SET messages = CONCAT(messages,'\n + " + firstName
+                    + " " + surname + " has changed their doctor to " + newDoctorName
+                    + ".') WHERE patientID = '" + Info.backgroundID + "';");
+	        
+	        
+	        }
+	        catch (Exception e) {
+	        	
+	        	assertEquals(expectedMessage, "Your doctor has successfully been changed to " + newDoctorName + ".");
+	        
+	        }
+
+	        
+	    }
+	 
+	 @Test
+	 public void testNotChangeDoctor() throws Exception {
+	        // Set up initial state
+	       
+	        String doctorName = "Dr. Johnson";
+	        String backgroundID = "789";
+	        String firstName = "John";
+	        String surname = "Doe";
+	        String expectedMessage = "Doctor does not exist!";
+
+	        
+
+	        // Call the method
+	        Info.backgroundID = backgroundID;
+	        try {
+	        	
+	        	//statement = connection.createStatement();
+	        //Checking if it can change doctor if all requirements are hit	
+	        	Info.statement.executeUpdate("use NHS");
+	            ResultSet resultSet = Info.statement.executeQuery("SELECT Name FROM doctors WHERE Name = '" + doctorName + "';");
+	        
+	        
+	        }
+	        catch (Exception e) {
+	        	
+	        	assertEquals(resultSet, "Doctor does not exist!");
+	        
+	        }
+
+	        
+	    }
+
+}
---
+++ Tests/ViewBookingTest.java
@@ -0,0 +1,70 @@
+package kent.comp5590;
+
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import kent.comp5590.Info;
+
+/**
+ * @author Callum
+ */
+public class ViewBookingTest {
+	
+	 public static Connection connection;
+	 public static Statement statement;
+
+	@Before
+	public void setUp() {
+		
+		try {
+			 Class.forName("com.mysql.cj.jdbc.Driver");
+
+	         DatabaseConnectionFunc.main(null);
+
+	         // Initialize the database
+	         if (DatabaseConnectionFunc.connected) {
+	             DatabaseDB.main(null);
+	             
+	             connection = DatabaseConnectionFunc.getConnection();
+                 statement = connection.createStatement();
+                 
+                 Info.connect();
+	         }
+			  } catch (Exception e) {
+		            e.printStackTrace();
+		        }
+	}
+
+	@Test
+	public void testViewBooking() throws SQLException {
+		String time = "12:00";
+		int doctorID = 1;
+		String date = "2024-05-01";
+		
+		String yearAndMonth = "2024-05";
+		
+		try {
+        //Add booking for time and date
+        ResultSet testDocAvailability = Info.statement.executeQuery("SELECT * FROM bookings WHERE DoctorID = '" + doctorID + "' AND Time = '"
+            + time + "' AND Date = '" + date + "';");
+        
+        //Query to check if there is a booking made in the same timeframe
+        ResultSet results = statement.executeQuery(
+                "SELECT DoctorID, Date, Time FROM bookings WHERE Date LIKE '%" + yearAndMonth + "%';");
+		}
+	 catch (Exception e) {
+        e.printStackTrace();
+    }
+        //assertEquals(testDocAvailability, results);
+        
+        
+	}
+
+}
