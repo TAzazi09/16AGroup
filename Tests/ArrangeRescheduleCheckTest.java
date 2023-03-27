96c16eca8f302233d7298b9a22a226a10dfb99d6
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2023-03-27 22:42:03 +0100

    Revert "I've spent 4 hours trying to make it work"
    This reverts commit 2c89f9b67ed74a8f1f215ecf518ddb0cdca65bc6.

---
+++ Tests/ArrangeRescheduleCheckTest.java
@@ -0,0 +1,34 @@
+package Tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+import javax.swing.JOptionPane;
+
+import Functionality.*;
+
+class ArrangeRescheduleCheckTest {
+	
+	@Test
+	void CorrectInput() {
+		String time = "08:00";
+		String date = "2024-05-05";
+		System.out.println(time.length());
+		
+		 JOptionPane optionPaneMock = mock(ArrangeRescheduleCheck.class);
+		
+		ArrangeRescheduleCheck.test(time, date);
+		
+		//assertFalse(ArrangeRescheduleCheck.test("1", "2024"));
+		
+		
+		//ArrangeRescheduleCheck.timeCheck("12:00");
+	}
+	
+	
+}
+
+
+
---
+++ Tests/ViewBookingTest.java
@@ -0,0 +1,73 @@
+package Tests;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import Functionality.*;
+import Databases.*;
+import Session.*;
+
+// import kent.comp5590.Info;
+
+/**
+ * @author Callum
+ */
+
+public class ViewBookingTest {
+	
+	 public static Connection connection;
+	 public static Statement statement;
+
+	@Before
+	public void setUp() {
+		
+		try {
+			 Class.forName("com.mysql.cj.jdbc.Driver");
+
+	         DatabaseConnectionFunc.main(null);
+
+	         // Initialize the database
+	         if (DatabaseConnectionFunc.connected) {
+	             DatabaseDB.main(null);
+	             
+	             connection = DatabaseConnectionFunc.getConnection();
+                 statement = connection.createStatement();
+                 
+                 Info.connect();
+	         }
+			  } catch (Exception e) {
+		            e.printStackTrace();
+		        }
+	}
+
+	@Test
+	public void testViewBooking() throws SQLException {
+		String time = "12:00";
+		int doctorID = 1;
+		String date = "2024-05-01";
+		
+		String yearAndMonth = "2024-05";
+		
+		try {
+        //Add booking for time and date
+        ResultSet testDocAvailability = Info.statement.executeQuery("SELECT * FROM bookings WHERE DoctorID = '" + doctorID + "' AND Time = '"
+            + time + "' AND Date = '" + date + "';");
+        
+        //Query to check if there is a booking made in the same timeframe
+        ResultSet results = statement.executeQuery(
+                "SELECT DoctorID, Date, Time FROM bookings WHERE Date LIKE '%" + yearAndMonth + "%';");
+		}
+	 catch (Exception e) {
+        e.printStackTrace();
+    }
+        //assertEquals(testDocAvailability, results);
+        
+        
+	}
+
+}
