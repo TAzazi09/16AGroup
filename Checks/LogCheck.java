2a876303a5140713acc787671ebc10344e434756
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2023-03-28 19:10:17 +0100

    Add the bolierplate for the checks for next time

--- Checks/LogCheck.java
+++ Checks/LogCheck.java
@@ -6,4 +6,28 @@ public class LogCheck {
 
         return true;
     }
+
+    public static boolean checkChangeDoctor(int patientID, String newDoctorName) {
+        // TODO
+
+        return true;
+    }
+
+    public static boolean checkLogin(int patientID) {
+        // TODO
+
+        return true;
+    }
+
+    public static boolean checkArrangeBooking(int patientID) {
+        // TODO
+
+        return true;
+    }
+
+    public static boolean checkRearrangeBooking(int patientID) {
+        // TODO
+
+        return true;
+    }
 }
