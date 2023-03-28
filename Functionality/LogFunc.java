6ecc8564f337767f453d9aa0419960d17a0137b5
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2023-03-28 19:09:14 +0100

    Add the boilerplate for the different logs to do next time

--- Functionality/LogFunc.java
+++ Functionality/LogFunc.java
@@ -6,10 +6,55 @@ import Checks.LogCheck;
 import Databases.PatientsDB;
 
 public class LogFunc {
+    // Logs the registration of a patient if it passes the check
     public static void logRegistration(int patientID) {
         if (LogCheck.checkRegistration(patientID)) {
             try {
                 LogsDB.insertLog(patientID, "Registered with doctor " + PatientsDB.getDoctorID(patientID));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // TODO
+    public static void logChangeDoctor(int patientID, String newDoctorName) {
+        if (LogCheck.checkChangeDoctor(patientID, newDoctorName)) {
+            try {
+                LogsDB.insertLog(patientID, "Changed doctor to " + newDoctorName);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // TODO
+    public static void logLogin(int patientID) {
+        if (LogCheck.checkLogin(patientID)) {
+            try {
+                LogsDB.insertLog(patientID, "Logged in");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // TODO
+    public static void logArrangeBooking(int patientID) {
+        if (LogCheck.checkArrangeBooking(patientID)) {
+            try {
+                LogsDB.insertLog(patientID, "Arranged a booking");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // TODO
+    public static void logRearrangeBooking(int patientID) {
+        if (LogCheck.checkRearrangeBooking(patientID)) {
+            try {
+                LogsDB.insertLog(patientID, "Rearranged a booking");
             } catch (Exception e) {
                 e.printStackTrace();
             }
