df891577fcefc91cadd7066d7dfd818aedb46649
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2023-03-28 18:33:58 +0100

    Create LogCheck.java

---
+++ Checks/LogCheck.java
@@ -0,0 +1,9 @@
+package Checks;
+
+public class LogCheck {
+    public static boolean checkRegistration(int patientID) {
+        // Currently all the checks are done in the RegistrationCheck class
+
+        return true;
+    }
+}
