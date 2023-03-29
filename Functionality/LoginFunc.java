0c4eef2fb14354a9dbfeff986f6a320d5f3e34fe
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2023-03-29 16:07:41 +0100

    Create LoginFunc to handle the functionality behind logging in

---
+++ Functionality/LoginFunc.java
@@ -0,0 +1,44 @@
+package Functionality;
+
+// imports from the java library
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
+
+// imports from the project
+import Checks.LoginCheck;
+import GUIs.MenuPage;
+import Session.*;
+
+/**
+ * @author Nikola
+ */
+public class LoginFunc {
+    public static int doctorName;
+    public static boolean userFound = false;
+
+    public static void sendData(String username, String password) {
+        try {
+            //Returns all tuples where the username matches
+            ResultSet resultSet = Info.statement
+                    .executeQuery("select * from patients WHERE FirstName = '" + username + "'");
+
+            if (LoginCheck.test(username, password, resultSet)) {
+                Info.backgroundID = resultSet.getString("patientID");
+                Info.firstname = resultSet.getString("FirstName");
+                Info.surname = resultSet.getString("Surname");
+
+                doctorName = resultSet.getInt("doctorID");
+                JOptionPane.showMessageDialog(null, "Welcome to the NHS!");
+
+                General.closeAllWindows();
+
+                userFound = true;
+
+                //Returns the user to the main menu
+                MenuPage.main(null);
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error: " + e.getMessage());
+        }
+    }
+}
