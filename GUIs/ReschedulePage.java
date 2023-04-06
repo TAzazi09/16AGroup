fdafcb938b131b0d8490c055bbaf74b3c1af06fc
Ethan Teather <et365@kent.ac.uk>
2023-04-06 12:24:34 +0100

    Added back button and checks for date / time input

--- GUIs/ReschedulePage.java
+++ GUIs/ReschedulePage.java
@@ -1,5 +1,10 @@
 package GUIs;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import javax.swing.JOptionPane;
+
 // imports from the project
 import Functionality.ReschedulingFunc;
 import Info.General;
@@ -9,99 +14,144 @@ import Info.Session;
  * @author Ethan
  */
 public class ReschedulePage extends javax.swing.JFrame {
-    // Variables declaration
-    private javax.swing.JTextField dateInput;
-    private javax.swing.JTextField timeInput;
+        // Variables declaration
+        private javax.swing.JTextField dateInput;
+        private javax.swing.JTextField timeInput;
 
-    public ReschedulePage(String oldDate, String oldTime) {
-        initComponents(oldDate, oldTime);
-    }
-
-    public static void loadPage(String oldDate, String oldTime) {
-        if (Session.userID != -1) {
-            General.setNimbusLookAndFeel(ReschedulePage.class);
-
-            /* Create and display the form */
-            java.awt.EventQueue.invokeLater(() -> new ReschedulePage(oldDate, oldTime).setVisible(true));
-        } else {
-            GeneralPage.main(null);
+        public ReschedulePage(String oldDate, String oldTime) {
+                initComponents(oldDate, oldTime);
         }
-    }
 
-    private void initComponents(String oldDate, String oldTime) {
+        public static void loadPage(String oldDate, String oldTime) {
+                if (Session.userID != -1) {
+                General.setNimbusLookAndFeel(ReschedulePage.class);
 
-        javax.swing.JLabel dateLabel = new javax.swing.JLabel();
-        timeInput = new javax.swing.JTextField("HH:MM");
-        dateInput = new javax.swing.JTextField("YYYY-MM-DD");
-        javax.swing.JLabel timeLabel = new javax.swing.JLabel();
-        javax.swing.JButton submit = new javax.swing.JButton();
-        javax.swing.JLabel title = new javax.swing.JLabel();
+                /* Create and display the form */
+                java.awt.EventQueue.invokeLater(() -> new ReschedulePage(oldDate, oldTime).setVisible(true));
+                } else {
+                GeneralPage.main(null);
+                }
+        }
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        private void initComponents(String oldDate, String oldTime) {
 
-        dateLabel.setText("New Date");
+                javax.swing.JLabel dateLabel = new javax.swing.JLabel();
+                timeInput = new javax.swing.JTextField("HH:MM");
+                dateInput = new javax.swing.JTextField("YYYY-MM-DD");
+                javax.swing.JLabel timeLabel = new javax.swing.JLabel();
+                javax.swing.JButton submit = new javax.swing.JButton();
+                javax.swing.JLabel title = new javax.swing.JLabel();
+                javax.swing.JButton back = new javax.swing.JButton();
 
-        timeLabel.setText("New Time");
+                setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        submit.setText("Reschedule");
-        submit.addActionListener(evt -> submitButton(evt, oldDate, oldTime));
+                dateLabel.setText("New Date");
 
-        title.setText("Please enter your new date and time:");
+                timeLabel.setText("New Time");
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(layout.createSequentialGroup()
-                                .addGap(71, 71, 71)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                        .addComponent(title)
-                                        .addComponent(submit)
-                                        .addGroup(layout.createSequentialGroup()
-                                                .addGroup(layout
-                                                        .createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                                        .addComponent(timeLabel)
-                                                        .addComponent(dateLabel))
-                                                .addGap(56, 56, 56)
-                                                .addGroup(layout
-                                                        .createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                                        .addComponent(timeInput,
-                                                                javax.swing.GroupLayout.Alignment.LEADING,
-                                                                javax.swing.GroupLayout.DEFAULT_SIZE, 75,
-                                                                javax.swing.GroupLayout.PREFERRED_SIZE)
-                                                        .addComponent(dateInput,
-                                                                javax.swing.GroupLayout.Alignment.LEADING,
-                                                                javax.swing.GroupLayout.DEFAULT_SIZE, 75,
-                                                                javax.swing.GroupLayout.PREFERRED_SIZE))))
-                                .addContainerGap(135, Short.MAX_VALUE)));
-        layout.setVerticalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(layout.createSequentialGroup()
-                                .addGap(25, 25, 25)
-                                .addComponent(title)
-                                .addGap(18, 18, 18)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                                        .addComponent(timeLabel)
-                                        .addComponent(timeInput, javax.swing.GroupLayout.PREFERRED_SIZE,
-                                                javax.swing.GroupLayout.DEFAULT_SIZE,
-                                                javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                                        .addComponent(dateLabel)
-                                        .addComponent(dateInput, javax.swing.GroupLayout.PREFERRED_SIZE,
-                                                javax.swing.GroupLayout.DEFAULT_SIZE,
-                                                javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addGap(27, 27, 27)
-                                .addComponent(submit)
-                                .addContainerGap(135, Short.MAX_VALUE)));
+                back.setText("Back");
+                back.setFont(General.font(18));
+                back.setBackground(General.BUTTON_BLUE);
+                back.addActionListener(evt -> {
+                        dispose();
+                });
 
-        pack();
-    }
+                submit.setText("Reschedule");
+                submit.setFont(General.font(18));
+                submit.setBackground(General.BUTTON_BLUE);
+                submit.addActionListener(evt -> submitButton(evt, oldDate, oldTime));
 
-    private void submitButton(java.awt.event.ActionEvent evt, String oldDate, String oldTime) {
-        String time = timeInput.getText();
-        String date = dateInput.getText();
-        // Sends both the new and old date / time
-        ReschedulingFunc.sendData(oldDate, oldTime, date, time);
-    }
+                title.setText("Please enter your new date and time:");
+                title.setFont(General.font(24));
+                
+
+                javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+                getContentPane().setLayout(layout);
+                layout.setHorizontalGroup(
+                                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                                .addGroup(layout.createSequentialGroup()
+                                                                .addGap(71, 71, 71)
+                                                                .addGroup(layout.createParallelGroup(
+                                                                                javax.swing.GroupLayout.Alignment.LEADING)
+                                                                                .addComponent(title)
+                                                                                .addComponent(submit)
+                                                                                .addGroup(layout.createSequentialGroup()
+                                                                                                .addGroup(layout
+                                                                                                                .createParallelGroup(
+                                                                                                                                javax.swing.GroupLayout.Alignment.LEADING)
+                                                                                                                .addComponent(back)
+                                                                                                                .addComponent(timeLabel)
+                                                                                                                .addComponent(dateLabel))
+                                                                                                .addGap(56, 56, 56)
+                                                                                                .addGroup(layout
+                                                                                                                .createParallelGroup(
+                                                                                                                                javax.swing.GroupLayout.Alignment.LEADING)
+                                                                                                                .addComponent(timeInput,
+                                                                                                                                javax.swing.GroupLayout.Alignment.LEADING,
+                                                                                                                                javax.swing.GroupLayout.DEFAULT_SIZE,
+                                                                                                                                75,
+                                                                                                                                javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                                                                                .addComponent(dateInput,
+                                                                                                                                javax.swing.GroupLayout.Alignment.LEADING,
+                                                                                                                                javax.swing.GroupLayout.DEFAULT_SIZE,
+                                                                                                                                75,
+                                                                                                                                javax.swing.GroupLayout.PREFERRED_SIZE))))
+                                                                .addContainerGap(135, Short.MAX_VALUE)));
+                layout.setVerticalGroup(
+                                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                                .addGroup(layout.createSequentialGroup()
+                                                                .addGap(25, 25, 25)
+                                                                .addComponent(title)
+                                                                .addGap(18, 18, 18)
+                                                                .addGroup(layout.createParallelGroup(
+                                                                                javax.swing.GroupLayout.Alignment.BASELINE)
+                                                                                .addComponent(timeLabel)
+                                                                                .addComponent(timeInput,
+                                                                                                javax.swing.GroupLayout.PREFERRED_SIZE,
+                                                                                                javax.swing.GroupLayout.DEFAULT_SIZE,
+                                                                                                javax.swing.GroupLayout.PREFERRED_SIZE))
+                                                                .addPreferredGap(
+                                                                                javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                                                .addGroup(layout.createParallelGroup(
+                                                                                javax.swing.GroupLayout.Alignment.BASELINE)
+                                                                                .addComponent(dateLabel)
+                                                                                .addComponent(dateInput,
+                                                                                                javax.swing.GroupLayout.PREFERRED_SIZE,
+                                                                                                javax.swing.GroupLayout.DEFAULT_SIZE,
+                                                                                                javax.swing.GroupLayout.PREFERRED_SIZE))
+                                                                .addGap(27, 27, 27)
+                                                                .addGroup(layout.createParallelGroup(
+                                                                                javax.swing.GroupLayout.Alignment.BASELINE)
+                                                                            
+                                                                                .addComponent(submit))
+                                                                                .addComponent(back)
+                                                                .addContainerGap(135, Short.MAX_VALUE)));
+
+                pack();
+        }
+
+        private void submitButton(java.awt.event.ActionEvent evt, String oldDate, String oldTime) {
+                String time = timeInput.getText();
+                String date = dateInput.getText();
+
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
+
+                java.util.Date correctTime = null;
+                java.util.Date correctDate = null;
+
+                dateFormat.setLenient(false);
+                timeFormat.setLenient(false);
+
+                try {
+                        correctDate = dateFormat.parse(date);
+                        correctTime = timeFormat.parse(time);
+                } catch (ParseException e) {}
+                // Checks if the inputs are ok
+                if ((correctTime == null) || (correctDate == null)) {
+                        JOptionPane.showMessageDialog(null, "Invalid year!");
+                } else
+                        // Sends both the new and old date / time
+                        ReschedulingFunc.sendData(oldDate, oldTime, date, time);
+        }
 }
