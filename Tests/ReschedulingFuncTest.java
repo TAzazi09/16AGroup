52adc8bb7cd9fcbf1f61aebb324a996b695b4839
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2023-04-07 16:38:22 +0100

    Auto-formatting in the Tests package

--- Tests/ReschedulingFuncTest.java
+++ Tests/ReschedulingFuncTest.java
@@ -19,104 +19,104 @@ import Info.*;
  */
 public class ReschedulingFuncTest {
 
-	@Before
-	public void setUp() throws Exception {
-		//ArrangeBookingFunc a = new ArrangeBookingFunc();
-		//kent.comp5590.ArrangeBookingFunc myClassMock = Mockito.mock(ArrangeBookingFunc.class);
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
+    @Before
+    public void setUp() throws Exception {
+        //ArrangeBookingFunc a = new ArrangeBookingFunc();
+        //kent.comp5590.ArrangeBookingFunc myClassMock = Mockito.mock(ArrangeBookingFunc.class);
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
 
-			DBConnectionFunc.main(null);
+            DBConnectionFunc.main(null);
 
-			// Initialize the database
-			if (DBConnectionFunc.connected) {
-				DatabaseDB.initDB();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+            // Initialize the database
+            if (DBConnectionFunc.connected) {
+                DatabaseDB.initDB();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-		//ResultSet selectDoc = Info.statement.executeQuery("SELECT DoctorID FROM Patients WHERE patientID = '1';");
-	}
+        //ResultSet selectDoc = Info.statement.executeQuery("SELECT DoctorID FROM Patients WHERE patientID = '1';");
+    }
 
-	@Test
-	public void testOverall() {
-		String oldDate = "2023-04-01";
-		String oldTime = "12:00";
-		String newDate = "2023-05-01";
-		String newTime = "11:00";
-		int docID = 1;
+    @Test
+    public void testOverall() {
+        String oldDate = "2023-04-01";
+        String oldTime = "12:00";
+        String newDate = "2023-05-01";
+        String newTime = "11:00";
+        int docID = 1;
 
-		try {
-			ResultSet results = Session.statement
-					.executeQuery("SELECT * FROM Bookings WHERE Time = '" + newTime + "' AND DoctorID = '"
-							+ docID + "' AND Date = '" + newDate + "'");
+        try {
+            ResultSet results = Session.statement
+                    .executeQuery("SELECT * FROM Bookings WHERE Time = '" + newTime + "' AND DoctorID = '"
+                            + docID + "' AND Date = '" + newDate + "'");
 
-			//Creating mock object for JOptionPane
-			JOptionPane mockOptionPane = mock(JOptionPane.class);
+            //Creating mock object for JOptionPane
+            JOptionPane mockOptionPane = mock(JOptionPane.class);
 
-			//Do nothing with mock object and return value from mock JOptionPane
-			Mockito.doNothing().when(mockOptionPane).showMessageDialog(null, Mockito.anyString());
+            //Do nothing with mock object and return value from mock JOptionPane
+            Mockito.doNothing().when(mockOptionPane).showMessageDialog(null, Mockito.anyString());
 
-			//Calling resechduleBooking with date and time
+            //Calling resechduleBooking with date and time
 
-			kent.comp5590.ReschedulingFunc.sendData(oldDate, oldTime, newDate, newTime);
+            kent.comp5590.ReschedulingFunc.sendData(oldDate, oldTime, newDate, newTime);
 
-			Mockito.verify(mockOptionPane);
-			//Checking to see if same message was output once method was called with values
+            Mockito.verify(mockOptionPane);
+            //Checking to see if same message was output once method was called with values
 
-			mockOptionPane.showMessageDialog(null,
-					"Your booking has successfully been changed to " + newTime + " on the " + newDate + ".");
+            mockOptionPane.showMessageDialog(null,
+                    "Your booking has successfully been changed to " + newTime + " on the " + newDate + ".");
 
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
-	@Test
-	public void testClash() {
-		String oldDate = "2023-04-01";
-		String oldTime = "12:00";
-		String newDate = "2023-05-01";
-		String newTime = "11:00";
+    @Test
+    public void testClash() {
+        String oldDate = "2023-04-01";
+        String oldTime = "12:00";
+        String newDate = "2023-05-01";
+        String newTime = "11:00";
 
-		String date = "2023-05-01";
-		String time = "11:00";
-		int docID = 1;
-		String curDoc = "Dr Jason";
-		int curPatient = 1;
+        String date = "2023-05-01";
+        String time = "11:00";
+        int docID = 1;
+        String curDoc = "Dr Jason";
+        int curPatient = 1;
 
-		int otherPatient = 2;
+        int otherPatient = 2;
 
-		try {
-			//Populating table with booking already at desired data and time for reschedule with other patient
-			Session.statement.execute(
-					"INSERT INTO Bookings (PatientID, DoctorID, Time, Date) VALUES ('" + otherPatient
-							+ "', '" + docID + "', '" + time + "', '" + date + "');");
-			//Query to check for availability of Doctor
-			ResultSet results = Session.statement
-					.executeQuery("SELECT * FROM Bookings WHERE Time = '" + newTime + "' AND DoctorID = '"
-							+ docID + "' AND Date = '" + newDate + "'");
+        try {
+            //Populating table with booking already at desired data and time for reschedule with other patient
+            Session.statement.execute(
+                    "INSERT INTO Bookings (PatientID, DoctorID, Time, Date) VALUES ('" + otherPatient
+                            + "', '" + docID + "', '" + time + "', '" + date + "');");
+            //Query to check for availability of Doctor
+            ResultSet results = Session.statement
+                    .executeQuery("SELECT * FROM Bookings WHERE Time = '" + newTime + "' AND DoctorID = '"
+                            + docID + "' AND Date = '" + newDate + "'");
 
-			//Creating mock object for JOptionPane
-			JOptionPane mockOptionPane = mock(JOptionPane.class);
+            //Creating mock object for JOptionPane
+            JOptionPane mockOptionPane = mock(JOptionPane.class);
 
-			//Do nothing with mock object and return value from mock JOptionPane
-			Mockito.doNothing().when(mockOptionPane).showMessageDialog(null, Mockito.anyString());
+            //Do nothing with mock object and return value from mock JOptionPane
+            Mockito.doNothing().when(mockOptionPane).showMessageDialog(null, Mockito.anyString());
 
-			//Calling resechduleBooking with date and time
+            //Calling resechduleBooking with date and time
 
-			kent.comp5590.ReschedulingFunc.sendData(oldDate, oldTime, newDate, newTime);
+            kent.comp5590.ReschedulingFunc.sendData(oldDate, oldTime, newDate, newTime);
 
-			Mockito.verify(mockOptionPane);
-			//Checking to see if same message was output once method was called with values
+            Mockito.verify(mockOptionPane);
+            //Checking to see if same message was output once method was called with values
 
-			mockOptionPane.showMessageDialog(null, curDoc + " is unavailable at that time.");
+            mockOptionPane.showMessageDialog(null, curDoc + " is unavailable at that time.");
 
-		} catch (Exception e) {
+        } catch (Exception e) {
 
-			e.printStackTrace();
-		}
-	}
+            e.printStackTrace();
+        }
+    }
 }
--- Tests/ViewBookingTest.java
+++ Tests/ViewBookingTest.java
@@ -17,50 +17,50 @@ import Databases.*;
  */
 public class ViewBookingTest {
 
-	public static Connection connection;
-	public static Statement statement;
+    public static Connection connection;
+    public static Statement statement;
 
-	@Before
-	public void setUp() {
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
+    @Before
+    public void setUp() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
 
-			DBConnectionFunc.main(null);
+            DBConnectionFunc.main(null);
 
-			// Initialize the database
-			if (DBConnectionFunc.connected) {
-				DatabaseDB.initDB();
+            // Initialize the database
+            if (DBConnectionFunc.connected) {
+                DatabaseDB.initDB();
 
-				connection = DBConnectionFunc.getConnection();
-				statement = connection.createStatement();
+                connection = DBConnectionFunc.getConnection();
+                statement = connection.createStatement();
 
-				Session.connect();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+                Session.connect();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	@Test
-	public void testViewBooking() throws SQLException {
-		String time = "12:00";
-		int doctorID = 1;
-		String date = "2024-05-01";
+    @Test
+    public void testViewBooking() throws SQLException {
+        String time = "12:00";
+        int doctorID = 1;
+        String date = "2024-05-01";
 
-		String yearAndMonth = "2024-05";
+        String yearAndMonth = "2024-05";
 
-		try {
-			//Add booking for time and date
-			ResultSet testDocAvailability = Session.statement
-					.executeQuery("SELECT * FROM Bookings WHERE DoctorID = '" + doctorID + "' AND Time = '"
-							+ time + "' AND Date = '" + date + "';");
+        try {
+            //Add booking for time and date
+            ResultSet testDocAvailability = Session.statement
+                    .executeQuery("SELECT * FROM Bookings WHERE DoctorID = '" + doctorID + "' AND Time = '"
+                            + time + "' AND Date = '" + date + "';");
 
-			//Query to check if there is a booking made in the same timeframe
-			ResultSet results = statement.executeQuery(
-					"SELECT DoctorID, Date, Time FROM Bookings WHERE Date LIKE '%" + yearAndMonth + "%';");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		//assertEquals(testDocAvailability, results);
-	}
+            //Query to check if there is a booking made in the same timeframe
+            ResultSet results = statement.executeQuery(
+                    "SELECT DoctorID, Date, Time FROM Bookings WHERE Date LIKE '%" + yearAndMonth + "%';");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //assertEquals(testDocAvailability, results);
+    }
 }
